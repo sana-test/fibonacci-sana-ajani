a5e6931bf539f5c3b69acd92508234faae5deb65
Sana Ajani <sajani6@gatech.edu>
2020-03-11 14:10:34 -0400

    Create fib2.py

---
+++ fib2.py
@@ -0,0 +1,15 @@
+numTerms = int(input("How many terms of Fibonacci sequence to print? "))
+
+def fibonnaci(n):
+   if n <= 1:
+       return n
+   else:
+       return(fibonnaci(n-1) + fibonnaci(n-2))
+
+# check if the number of terms is valid
+if numTerms <= 0:
+   print("Please enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(numTerms):
+       print(fibonnaci(i))
